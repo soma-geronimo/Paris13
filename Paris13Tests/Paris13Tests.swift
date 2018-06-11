e48d8923a5ea781d123ba2e32acfb90f7703e2e9
soma-geronimo <s.bini@groupeonepoint.com>
2018-06-11 12:42:26 +0200

    Initial Commit

---
+++ Paris13Tests/Paris13Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Paris13Tests.swift
+//  Paris13Tests
+//
+//  Created by Soma Bini on 11/06/2018.
+//  Copyright © 2018 Soma Bini. All rights reserved.
+//
+
+import XCTest
+@testable import Paris13
+
+class Paris13Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
